00592f399baca916796a34bccf3a612d82bde1e0
DuyKhang070903 <ankhangtn2003s@gmail.com>
2024-12-30 23:24:22 +0700

    Learn Ros

---
+++ devel/share/my_robot_msgs/cmake/my_robot_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(my_robot_msgs_MESSAGE_FILES "/home/duykhang0709/catkin_ws/src/my_robot_msgs/msg/HardwareStatus.msg")
+set(my_robot_msgs_SERVICE_FILES "/home/duykhang0709/catkin_ws/src/my_robot_msgs/srv/ComputeDiskArea.srv;/home/duykhang0709/catkin_ws/src/my_robot_msgs/srv/SetLed.srv")
